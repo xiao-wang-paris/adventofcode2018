35aeb121b510edbfde1b2ca9a9d5bb88b80192b7
Xiao Wang <xiawang@microsoft.com>
2018-12-11 11:45:01 +0100

    day2

--- AdventCalendar/Program.cs
+++ AdventCalendar/Program.cs
@@ -1,16 +1,35 @@
 using System;
 using AdventCalendar.day1;
+using AdventCalendar.day2;
 
 namespace AdventCalendar
 {
     class Program
     {
-        static void Main(string[] args)
+        static void SolvePuzzleDay1()
         {
-            var inputPath =@"C:\Users\xiawang\source\repos\AdventCalendar\AdventCalendar\day1\input.txt";
+            var basePath = System.IO.Directory.GetCurrentDirectory();
+            var inputPath = @"{basePath}\..\..\..\day1\input.txt";
             var computer = new Day1(inputPath);
             Console.WriteLine("question 1: " + computer.ComputeFrequence());
             Console.WriteLine("question 2: " + computer.ComputeFirstDuplicate());
+        }
+
+        static void SolvePuzzleDay2()
+        {
+            var basePath = System.IO.Directory.GetCurrentDirectory(); 
+            var inputPath = @"{basePath}\..\..\..\day2\input.txt";
+            //var mockInputPath = @"{basePath}\..\..\..\day2\mock.txt";
+            //var mock2InputPath = @"{basePath}\..\..\..\day2\mock2.txt";
+            //var solver = new PuzzleSolver(mock2InputPath);
+            //var solver = new PuzzleSolver(mockInputPath);
+            var solver = new PuzzleSolver(inputPath);
+            Console.WriteLine("question 1: " + solver.GetCheckSum());
+            Console.WriteLine("quesiton 2: " + solver.GetCommonString());
+        }
+        static void Main(string[] args)
+        {
+            SolvePuzzleDay2();
         }
     }
 }
---
+++ AdventCalendar/day2/PuzzleSolver.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Text;
+
+namespace AdventCalendar.day2
+{
+    public class RepeatCounter
+    {
+        public int DoubleCount { get; set; }
+        public int TripleCount { get; set; }
+    }
+
+    public class PuzzleSolver
+    {
+        private readonly string inputPath;
+        public PuzzleSolver(string path)
+        {
+            inputPath = path;
+        }
+
+        private RepeatCounter Count(string s)
+        {
+            int[] counter = new int[26];
+            var res = new RepeatCounter();
+            foreach(char c in s.ToCharArray())
+            {
+                counter[(c - 'a')]++;
+            }
+            foreach(int count in counter)
+            {
+                if(count == 2)
+                {
+                    res.DoubleCount = 1;
+                }else if(count == 3)
+                {
+                    res.TripleCount = 1;
+                }
+            }
+            return res;
+        }
+
+        //question 1 
+        public int GetCheckSum()
+        {
+            string[] ids = System.IO.File.ReadAllLines(inputPath);
+            var res = new RepeatCounter();
+            foreach (string line in ids)
+            {
+                var countId = Count(line);
+                res.DoubleCount += countId.DoubleCount;
+                res.TripleCount += countId.TripleCount;
+            }
+            return res.DoubleCount * res.TripleCount;
+        }
+
+        private int Distance(string s1, string s2)
+        {
+            if(s1.Length != s2.Length)
+                return Math.Min(s1.Length, s2.Length);
+
+            var dist = 0;
+            char[] arr1 = s1.ToCharArray();
+            char[] arr2 = s2.ToCharArray();
+
+            for (int i=0;i<s1.Length; i++)
+            {
+                if (arr1[i] != arr2[i])
+                    dist++; 
+            }
+            return dist;
+        }
+
+        private string CommonString(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+                return null;
+            StringBuilder common = new StringBuilder();
+            char[] arr1 = s1.ToCharArray();
+            char[] arr2 = s2.ToCharArray();
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (arr1[i] == arr2[i])
+                    common.Append(arr1[i]);
+            }
+            return common.ToString();
+        }
+
+        //question2 
+        public string GetCommonString()
+        {
+            string[] ids = System.IO.File.ReadAllLines(inputPath);
+            var res = new RepeatCounter();
+            for(int i=0; i<ids.Length; i++)
+            {
+                for(int j=i+1; j<ids.Length; j++)
+                {
+                    string id1 = ids[i];
+                    string id2 = ids[j];
+                    if(Distance(id1, id2) == 1)
+                    {
+                        return CommonString(id1, id2);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
